d461d14b9eff7c10149fa2fe98ef1584c007433b
Misha <vechnuignor@mail.ru>
2025-02-20 09:38:27 +0300

    addChangeOnMainPage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class App extends Component {
   render() {
     return (
       <div>
-        <div className='header'>
+        {/* <div className='header'>
           Phone Directory
         </div>
         <button> Add </button>
         <div>
           <span> Name </span><br/>
           <span> Phone </span>
-        </div>
+        </div> */}
+        <label htmlFor="name">Name:</label>
+        <input
+          id="name"
+          type="text"
+          placeholder="Type Here"
+          defaultValue="Shilpa"
+        />
       </div>
     );
   }
