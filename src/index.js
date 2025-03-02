156c8d00038fab75592729c90b0fedc5c4aa318e
Misha <vechnuignor@mail.ru>
2025-03-02 06:59:45 +0300

    addSmartComponent

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import './index.css';
 import ShowSubscribers from './ShowSubscribers';
 import reportWebVitals from './reportWebVitals';
 import AddSubscriber from './AddSubscriber';
+import PhoneDirectory from './PhoneDirectory';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ShowSubscribers />
+    <PhoneDirectory />
   </React.StrictMode>
 );
 
