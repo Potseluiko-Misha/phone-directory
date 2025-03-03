9cf8c35d6d131bf20558aab5b86559e24649bd7f
Misha <vechnuignor@mail.ru>
2025-03-03 18:01:29 +0300

    deleteTrash

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import ShowSubscribers from './ShowSubscribers';
 import reportWebVitals from './reportWebVitals';
-import AddSubscriber from './AddSubscriber';
 import PhoneDirectory from './PhoneDirectory';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
